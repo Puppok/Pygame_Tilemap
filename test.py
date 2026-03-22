9229ee2310a9f3156cbfeae93d7cfe7a170f52bd
unknown <g3b17rastorguevda@mail.ru>
2026-03-22 09:18:20 +0800

    function compare

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-name = 'Tom'
-age = -4
-print(f'Hello, {name}, you are {age} years old')
+# name = 'Tom'
+# age = -4
+# print(f'Hello, {name}, you are {age} years old')
 
-print('Im using Python with Git')
+# print('Im using Python with Git')
+
+a = int(input())
+b = int(input())
+if a > b:
+    print(f'{a} больше {b}')
+elif a == b:
+    print(f'{a} равно {b}')
+else:
+    print(f'{a} меньше {b}')
