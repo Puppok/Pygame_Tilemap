23acf83be2351b678411c76fde24259902562ad6
Захар <zaharmisin295@gmail.com>
2026-03-22 09:18:19 +0800

    function digit-sum

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-name = 'Tom'
-age = -4
-print(f'Hello, {name}, you are {age} years old')
+# name = 'Tom'
+# age = -4
+# print(f'Hello, {name}, you are {age} years old')
 
-print('Im using Python with Git')
+# print('Im using Python with Git')
+s = 0
+arr = list(input())
+for el in arr:
+    s += int(el)
+print(s)
