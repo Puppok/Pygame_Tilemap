20ef3f4b0a7f68dacce9adaf74cba7815c588c27
puppok <kravchenko19971808@gmail.com>
2026-03-22 09:15:08 +0800

    function table-multi

--- test.py
+++ test.py
@@ -3,3 +3,12 @@ age = -4
 print(f'Hello, {name}, you are {age} years old')
 
 print('Im using Python with Git')
+
+def table_multi():
+    print('\nTable Multi:')
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(i * j, end = '\t')
+        print()
+
+table_multi()
