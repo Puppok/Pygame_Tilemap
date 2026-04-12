dda7bfc32cc2d5de96a35a4a1c3d13f0b5fa4681
puppok <kravchenko19971808@gmail.com>
2026-04-12 09:04:38 +0800

    init pygame

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# init
+import pygame as pg
+from modules import player, tilemap
+
+# Инициализация
+pg.init()
+screen = pg.display.set_mode((800, 600))
+pg.display.set_caption("Tilemap Platformer")
+clock = pg.time.Clock()
